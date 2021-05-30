597b042f71cf1dcac748e4d565971d795e7f76b7
9ekaitz <ekaitz.casado@alumni.mondragon.edu>
2021-05-30 15:03:13 +0200

    Menuaren egitura nagusia sortuta, botoiak egitea falta da

--- GIO/src/ApplicationFrame.java
+++ GIO/src/ApplicationFrame.java
@@ -1,23 +1,36 @@
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.MouseInfo;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 
 public class ApplicationFrame extends JFrame{
 	
+	private final static String LOGO_PATH = "res/icon32.png";
 	JPanel mainPanel;
+	JPopupMenu userMenu;
 	
 	public ApplicationFrame() {
 		super("GIO");
 		this.setSize(new Dimension(1280,720));
 		this.setContentPane(createAppPanel());
 		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setVisible(true);
 	}
 	
@@ -26,12 +39,44 @@ public class ApplicationFrame extends JFrame{
 		JPanel subPanel = new JPanel();
 		BoxLayout b = new BoxLayout(subPanel, BoxLayout.X_AXIS);
 		subPanel.setLayout(b);
-		subPanel.add(new JButton("Button"));
+		subPanel.add(createLogoButton());
 		subPanel.add(Box.createHorizontalGlue());
-		subPanel.add(new JLabel("Texto"));
+		subPanel.add(createUserButton());
+		subPanel.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+		
+		userMenu = new JPopupMenu();
+		JMenuItem show = new JMenuItem("showwww");
+		userMenu.add(show);
+		this.add(userMenu);
 		mainPanel.add(subPanel, BorderLayout.NORTH);
-		mainPanel.add(new JLabel("Hello"), BorderLayout.CENTER);
+		mainPanel.add(new MainMenu(), BorderLayout.CENTER);
 		return mainPanel;
+	}
+	
+	
+
+	private Component createUserButton() {
+		JLabel user = new JLabel();
+		user.setIcon(new ImageIcon("res/user.png"));
+		user.setFocusable(false);
+		user.setBorder(null);
+		user.addMouseListener(new MouseAdapter() {
+			 @Override 
+			 public void mousePressed(MouseEvent e) {
+			      userMenu.show(ApplicationFrame.this, 1220, 70);
+			 }
+		});
+		return user;
+	}
+
+	private Component createLogoButton() {
+		JButton button = new JButton();
+		button.setIcon(new ImageIcon(LOGO_PATH));
+		button.setFocusable(false);
+		button.setBorder(null);
+		button.setBorderPainted(false);
+		button.setContentAreaFilled(false);
+		return button;
 	}
 
 	public void setPanel(JPanel panel) {
